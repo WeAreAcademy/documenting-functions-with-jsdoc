492a833d0d3c94ece73a7991451715e382fe317c
Richard Ng <richardcrng@gmail.com>
2021-06-01 21:36:16 +0100

    Small cosmetic change to testing output

--- examples/2-void-functions.js
+++ examples/2-void-functions.js
@@ -13,8 +13,10 @@ function addTwiceToArray(arrOfStrings, newString) {
 console.log("\n------ addTwiceToArray -------");
 let arrOne = ['hi', 'hello'];
 console.log("Given arrOne", arrOne);
-console.log("When executing addTwiceToArray(arrOne, 'hey')")
-addTwiceToArray(arrOne, 'hey');
+console.log(
+  "After executing addTwiceToArray(arrOne, 'hey'):",
+  addTwiceToArray(arrOne, "hey")
+);
 console.log("Then it should have been mutated:", arrOne, "should be ['hi', 'hello', 'hey', 'hey']");
 
 
@@ -31,6 +33,8 @@ function orderByDescending(arrOfNums) {
 console.log("\n------ orderByDescending -------");
 let arrTwo = [3, 1, 2, 4];
 console.log("Given arrTwo is", arrTwo);
-console.log("When executing orderByDescending(arrTwo)");
-orderByDescending(arrTwo);
+console.log(
+  "After executing orderByDescending(arrTwo):",
+  orderByDescending(arrTwo)
+);
 console.log("Then it should have been mutated:", arrTwo, "should be [4, 3, 2, 1]");
