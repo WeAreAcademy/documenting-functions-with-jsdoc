7a5ba5858c20a9f1470aa023592ab0fe04611449
Richard Ng <richardcrng@gmail.com>
2021-06-01 20:34:07 +0100

    Add function signature examples for exercise 1

---
+++ examples/1-function-signature-examples.js
@@ -0,0 +1,8 @@
+// string method examples
+"hello world".repeat();
+"hello world".toUpperCase();
+"hello world".indexOf();
+
+// array method examples
+["a", "b", "c", "d"].includes();
+[1, 2, 3, 4].includes();
